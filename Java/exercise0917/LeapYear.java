d3a83975dc8fcd59df6b7f4f3b853a67fc23fe51
hujingyuan <flashhu@163.com>
2019-09-23 22:01:37 +0800

    0917homework-java

---
+++ Java/exercise0917/LeapYear.java
@@ -0,0 +1,20 @@
+package exercise0917;
+
+import java.util.Scanner;
+
+//Page83 list3-7
+public class LeapYear {
+  public static void main(String[] args) {
+	//Create a Scanner
+	Scanner input = new Scanner(System.in);
+	System.out.print("Enter a year: ");
+	int year = input.nextInt();
+	
+	//Check if the year is a leap year
+	boolean isLeapYear = 
+	  (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+
+	//Display the result
+	System.out.println(year +  " is a leap year? " + isLeapYear);
+  }
+}
