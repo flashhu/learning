ad346aa6a450c4c22e3239564c7fe58874e327e8
flashhu <flashhu@163.com>
2020-04-06 19:21:29 +0800

    显示倒计时

--- server-LY/07Express/ex07/index.js
+++ server-LY/07Express/ex07/index.js
@@ -7,10 +7,10 @@ var hbs = require('express-handlebars');
 // var vaildator = require('validator.tool');
 // var v = new validator();
 var app = express();
-var time='01:30', end, nowtimer;
 const LEN = 4; //验证码长度
 // var Users = [];
 var code = '';
+const interval = 180000; //3min
 
 const User = require('./model/user');
 
@@ -28,44 +28,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(cookieParser());
 // app.use(session({secret: "My secret key"}));
 
-//处理倒计时
-function countDown(end, res) {
-	let now = Date.now();
-	let duration = (end - now) / 1000;
-	let minute = `${parseInt(duration / 60)}`;
-	let second = `${parseInt(duration) % 60}`;
-	let m = `00${minute}`.substr(minute.length);
-    let s = `00${second}`.substr(second.length);
-    time = m + ':' + s;
-    // console.log(time);
-    // res.render('signup', {showbtn: false, timer: time})
-    if (duration <= 1){
-		clearInterval(nowtimer);
-	}
-}
-
 app.get('/', (req, res) => {
-	if(code) {
-		res.render('signup', {showbtn: false});
+	if(code){
+	  res.render('signup', {showbtn: true});
 	}else {
-		res.render('signup', {showbtn: true});
+	  res.render('signup');
 	}
 })
 
 //点击获取验证码
 app.post('/sendcode', function(req, res){
-	// const interval = 180000; //3min
-	const interval = 5000;
-	const start = Date.now();
-	const end = start + interval;
-	// nowtimer = setInterval(()=>{countDown(end, res)}, 1000);
-	console.log('time', time);
-    // res.render('signup', {showbtn: false, timer: time});
     for(let i = 0;i < LEN;i ++){
     	code += `${Math.floor(Math.random()*10)}`; 
     }
 	console.log('code', code);
-	setTimeout(function () { code = '' }, interval);
+	// setTimeout(function () { code = '' }, interval);
     // req.session.code = code;
     res.redirect('/');
 })
@@ -150,7 +127,4 @@ app.listen(8000)
 //两个按钮POST的不同响应，做法参考文1，文2解法目前不会orz
 //https://www.cnblogs.com/xieyunc/p/9126449.html
 //https://cnodejs.org/topic/56ab08a771204e03637a36c0
-//处理倒计时后，setInterval中的time不会改变全局的time,导致不能显示逐秒变化的timer orz
-//res.render只能执行一次，放在setInterval中会报错，对于显示timer，或间隔3分钟后再出现获取验证码，没有使用这类方法的思路
-//使用setTimeout后，到时间需要刷新才会显示获取验证码按钮
 //分开两个路由处理之后，对一些参数传递处理，问题很多... 
